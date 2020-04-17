c41a9913a293026be2daf6ce7268195e12605d97
RahelaPogacean <33206716+RahelaPogacean@users.noreply.github.com>
2020-04-17 16:11:15 +0300

    Update IRestaurantProcessing.java

--- src/main/java/businessLayer/IRestaurantProcessing.java
+++ src/main/java/businessLayer/IRestaurantProcessing.java
@@ -8,18 +8,10 @@ public interface IRestaurantProcessing{
 
 	public void createMenuItem(MenuItem mitem);
 	
-	public static void createBaseItem(String n, int id, double p) {
-		
-	}
-	public static void createCompositeItem(String n, int id, double p, ArrayList<MenuItem> list) {
-		
-	}
-	public static void deleteMenuItem(int idd) {
-		
-	}
-	public static void editMenuItem(int idd, String newName, double newPrice) {
-		
-	}
+	public static void createBaseItem(String n, int id, double p);
+	public static void createCompositeItem(String n, int id, double p, ArrayList<MenuItem> list);
+	public static void deleteMenuItem(int idd);
+	public static void editMenuItem(int idd, String newName, double newPrice);
 	public void createOrder(Order order, int idd, int nrTable, ArrayList<MenuItem> list);
 
 	public static double computePrice(int idd) {
